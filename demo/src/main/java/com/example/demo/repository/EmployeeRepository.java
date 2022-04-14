8e95ea8751bfaf5067b6983cf42711030901e4b0
Chathura <chathura.madura11@gmail.com>
2022-04-15 01:05:35 +0530

    formatted

--- demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/*from model/Employee.java parameterized constructor */
 public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 
 }
