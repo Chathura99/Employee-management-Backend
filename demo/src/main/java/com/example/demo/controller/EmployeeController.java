8e6c152d83da0749f100a5524e872e346f13c114
Chathura <chathura.madura11@gmail.com>
2022-04-15 22:31:02 +0530

    update and delete methods

--- demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.example.demo.exception.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,17 +33,46 @@ public class EmployeeController {
     //create employee rest api
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
+
         return employeeRepository.save(employee);
     }
 
     //get employee by is rest api
     @GetMapping("/employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(()->new ResourceNotFoundException("Employee not found with id : "+ id));
     return ResponseEntity.ok(employee);
+
+    }
+    //update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee not found with id : "+ id));
+        //update details
+        employee.setUsername(employeeDetails.getUsername());
+        employee.setEmail(employeeDetails.getEmail());
+        Employee updatedEmployee = employeeRepository.save(employee);
+        //show in postman
+        return ResponseEntity.ok(updatedEmployee);
     }
 
+    //delete employee rest api
+    @DeleteMapping("/employees/{id}")
+    //@PathVariable used to map with /employees/{id}
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee not found with id : "+ id));
+
+        employeeRepository.delete(employee);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 
 
 }
